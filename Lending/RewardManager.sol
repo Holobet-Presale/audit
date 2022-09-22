3a149105584fc51c44d57e3a92a4d418b8c5d962
haemmerich <105878770+haemmerich@users.noreply.github.com>
2022-09-22 15:44:11 +0800

    initial commit for audit

---
+++ Lending/RewardManager.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+interface IHBTLendingStaking {
+    function userInfo(address)
+        external
+        view
+        returns (uint256 amount, bool hasTakenLoan);
+
+    function chargeInterest(address user, uint256 amount) external;
+
+    function manageLoan(address user, bool hasTakenLoan) external;
+
+    function confiscateStaking(address _user) external;
+}
+
+contract RewardManager is Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable REWARDTOKEN;
+    mapping(address => uint256) public rewards;
+    IHBTLendingStaking public immutable stakingContract;
+    address public immutable lendingManager;
+
+    event onRewardTopUp(address user, uint256 amount);
+    event onRewardPaid(uint256 amount);
+    event onRewardConfiscated(address user, uint256 amount);
+
+    constructor(
+        IERC20 _REWARDTOKEN,
+        IHBTLendingStaking _stakingContract,
+        address _lendingManager
+    ) {
+        REWARDTOKEN = _REWARDTOKEN;
+        stakingContract = _stakingContract;
+        lendingManager = _lendingManager;
+    }
+
+    function topUpRewards(address user, uint256 amount) public onlyOwner {
+        REWARDTOKEN.safeTransferFrom(msg.sender, address(this), amount);
+        rewards[user] += amount;
+        emit onRewardTopUp(user, amount);
+    }
+
+    function withdrawRewards() public {
+        uint256 rewardAmount = rewards[msg.sender];
+        require(rewardAmount > 0, "ZERO Rewards");
+        (, bool hasTakenLoan) = stakingContract.userInfo(msg.sender);
+        require(!hasTakenLoan, "Please Clear Loan First");
+        rewards[msg.sender] = 0;
+        REWARDTOKEN.safeTransfer(msg.sender, rewardAmount);
+        emit onRewardPaid(rewardAmount);
+    }
+
+    function confiscateRewards(address user) public {
+        require(msg.sender == lendingManager, "not authorized");
+        uint256 rewardAmount = rewards[user];
+        rewards[user] = 0;
+        REWARDTOKEN.safeTransfer(owner(), rewardAmount);
+
+        emit onRewardConfiscated(user, rewardAmount);
+    }
+
+
+
+    function withdrawStuckTokens(IERC20 token) public  onlyOwner{
+        token.safeTransfer(msg.sender, token.balanceOf(address(this)));
+
+    }
+}
---
+++ Lending/xHBT.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract xHBTToken is ERC20("xHBT", "xHBT"), Ownable {
+    mapping(address => bool) public minters;
+    mapping(address => bool) public transferWhitelist;
+
+    function mint(address user, uint256 amount) public {
+        require(minters[msg.sender], "address not minter");
+        _mint(user, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function manageMinters(address addr, bool isAdd) public onlyOwner {
+        minters[addr] = isAdd;
+    }
+
+    function manageTransferWhitelist(address addr, bool isAdd)
+        public
+        onlyOwner
+    {
+        transferWhitelist[addr] = isAdd;
+    }
+
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        require(
+            transferWhitelist[from] || transferWhitelist[to],
+            "Cannot Transfer to non whitelist address"
+        );
+        super._transfer(from, to, amount);
+    }
+}
